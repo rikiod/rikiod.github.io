3be221cfdf2e0a0d841b449f23a7f638076114f1
Rikio <rikio@berkeley.edu>
2023-11-25 13:49:23 -0800

    pulled  cname

--- rikiosroom/src/App.js
+++ rikiosroom/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import React from 'react';
 import Home from './pages/Home/Home.jsx'
 import About from './pages/About/About.jsx'
+import Footer from './components/Footer/Footer.jsx'
 
 function App() {
   return (
     <div className="App">
 
       <Home />
+      <Footer />
       {/* <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ rikiosroom/src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// If you have a CSS file for styling your Home component, import it here
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer-container">
+            <p className="level-0">index</p>
+            <p className="level-1">plants</p>
+            <p className="level-1">hobbies</p>
+           
+          
+        </div>
+    );
+};
+
+export default Footer;
