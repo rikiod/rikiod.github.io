3dfbfb2a63af7b289c00f234c1e60310579ab901
Rikio <rikio@berkeley.edu>
2025-07-18 12:19:11 -0700

    slight changes to about

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -5,8 +5,8 @@ const About: React.FC = () => {
     return (
         <div className="about-container">
             <h1>about me</h1>
-            <p>Hi! I'm a senior at UC Berkeley studying Computer Science and Public Health.</p>
-            <p>I like jumping into difficult problems, and finding simple but effective fixes. I'm seeking opportunities in software.</p>
+            <p>Hi! I'm a software engineer based in the Bay Area, with a background in Computer Science and Public Health from UC Berkeley.</p>
+            <p>I like jumping into difficult problems, and finding simple but effective fixes.</p>
             <p>Let's talk! Shoot me at message at rikio [@] berkeley [.] edu</p>
         </div>
     );
